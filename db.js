086772b34134da4447a88665eefc711fda7406ac
Beeplov Sharma <beeplovsharma@gmail.com>
2024-06-28 18:16:08 +0530

    PORT = 3000

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 
 const db = async () => {
-    try {
-        const connection = await mongoose.connect(
-          process.env.LOCAL_MONGO_DB_URI
-        );
-        console.log("Connected to MongoDB");
-        return connection;
-    } catch (error) {
-        throw new error;
-    }
-}
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("Connected to MongoDB");
+    return connection;
+  } catch (error) {
+    console.log("Error in connecting to DB", error);
+  }
+};
 
 export default db;
